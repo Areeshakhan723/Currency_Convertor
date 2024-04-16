6075eecd435c8803eeda879f3f6b440c7499cd2e
Areesha  Khan <160509293+Areeshakhan723@users.noreply.github.com>
2024-04-16 17:58:37 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,46 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+let currency: any = {
+    USD: 1,
+    EUR: 0.91,
+    GBP: 0.76,
+    INR: 74.57,
+    PKR: 280
+}
+
+
+let user_Ans = await inquirer.prompt([
+    {
+        name: "from",
+        message: "Enter from Currency",
+        type: "list",
+        choices:["USD", "EUR","GBP","INR","PKR"]
+    },
+    {
+        name: "to",
+        message: "Enter to Currency",
+        type: "list",
+        choices:["USD", "EUR","GBP","INR","PKR"]
+    },
+    {
+        name: "amount",
+        message: "Enter your amount",
+        type: "number"
+    }
+]);
+
+let fromAmount = currency[user_Ans.from];
+let toAmount = currency[user_Ans.to];
+let amount = user_Ans.amount;
+
+let baseAmount = amount / fromAmount;
+let convertedAmount = baseAmount * toAmount
+
+// console.log(fromAmount);
+// console.log(toAmount);
+// console.log(amount);
+
+let converte = Math.floor(convertedAmount);
+console.log(converte);
